0101efd9ccba463235ff78b095f11f1b43e1be4e
ccomkhj <ccomkhj@gmail.com>
2025-02-20 17:55:45 +0000

    recall f1 precision included

--- resnet_classifier.py
+++ resnet_classifier.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torchvision.models as models
 from torch.optim import SGD, Adam
 from torch.utils.data import DataLoader
-from torchmetrics import Accuracy
+from torchmetrics import Accuracy, Precision, Recall, F1Score
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
@@ -48,6 +48,9 @@ class ResNetClassifier(pl.LightningModule):
         # Loss and metrics
         self.loss_fn = nn.CrossEntropyLoss()
         self.acc = Accuracy(task="multiclass", num_classes=num_classes)
+        self.precision = Precision(task="multiclass", num_classes=num_classes, average='macro')
+        self.recall = Recall(task="multiclass", num_classes=num_classes, average='macro')
+        self.f1 = F1Score(task="multiclass", num_classes=num_classes, average='macro')
         
         # ResNet model setup
         self.resnet_model = self.resnets[resnet_version](pretrained=transfer)
@@ -68,7 +71,7 @@ class ResNetClassifier(pl.LightningModule):
         # Create the optimizer instance.
         optimizer = self.optimizer(self.parameters(), lr=self.lr)
         # Define a StepLR scheduler that decays the learning rate every 10 epochs by a factor of 0.1.
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
         # Return the optimizer and scheduler dictionary.
         return {
             "optimizer": optimizer,
@@ -84,10 +87,13 @@ class ResNetClassifier(pl.LightningModule):
         preds = self(x)
         loss = self.loss_fn(preds, y)
         acc = self.acc(preds, y)
-        return loss, acc
+        precision = self.precision(preds, y)
+        recall = self.recall(preds, y)
+        f1 = self.f1(preds, y)
+        return loss, acc, precision, recall, f1
 
     def _dataloader(self, data_path, shuffle=False):
-        resize_size = (1460 // 2, 1936 // 2)
+        resize_size = (1460, 1936)
         
         if shuffle:
             # For training data, introduce dynamic augmentation using RandAugment.
@@ -112,21 +118,30 @@ class ResNetClassifier(pl.LightningModule):
 
     # Training methods
     def training_step(self, batch, batch_idx):
-        loss, acc = self._step(batch)
-        self.log("train_loss", loss, prog_bar=True)
-        self.log("train_acc", acc, prog_bar=True)
+        loss, acc, precision, recall, f1 = self._step(batch)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("train_precision", precision, on_step=True, on_epoch=True)
+        self.log("train_recall", recall, on_step=True, on_epoch=True)
+        self.log("train_f1", f1, on_step=True, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, acc = self._step(batch)
-        self.log("val_loss", loss, prog_bar=True)
-        self.log("val_acc", acc, prog_bar=True)
+        loss, acc, precision, recall, f1 = self._step(batch)
+        self.log("val_loss", loss, on_epoch=True, prog_bar=True)
+        self.log("val_acc", acc, on_epoch=True, prog_bar=True)
+        self.log("val_precision", precision, on_epoch=True)
+        self.log("val_recall", recall, on_epoch=True)
+        self.log("val_f1", f1, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
-        loss, acc = self._step(batch)
-        self.log("test_loss", loss)
-        self.log("test_acc", acc)
-
+        loss, acc, precision, recall, f1 = self._step(batch)
+        self.log("test_loss", loss, on_epoch=True)
+        self.log("test_acc", acc, on_epoch=True)
+        self.log("test_precision", precision, on_epoch=True)
+        self.log("test_recall", recall, on_epoch=True)
+        self.log("test_f1", f1, on_epoch=True)
+    
     # Data loaders
     def train_dataloader(self):
         return self._dataloader(self.train_path, shuffle=True)
